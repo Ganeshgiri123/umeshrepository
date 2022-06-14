3ea6ac9fd895075f24546c9a95e59f693d85f388
unknown <umeshbaviskarus12@gmail.comgit config --global user.name 12usbaviskar>
2022-06-15 01:05:35 +0530

    task

---
+++ umeshphonebook/src/main/java/com/bikkadit/phonebook/UmeshphonebookApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadit.phonebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmeshphonebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmeshphonebookApplication.class, args);
+	}
+
+}
---
+++ umeshphonebook/src/test/java/com/bikkadit/phonebook/UmeshphonebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadit.phonebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmeshphonebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
